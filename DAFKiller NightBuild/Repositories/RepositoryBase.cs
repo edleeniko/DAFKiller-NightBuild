fd69e017fc28395f5e13b81901a486fc0139314c
EdLee <edleeniko@gmail.com>
2023-07-28 19:57:06 +0300

    #added connection strings

--- DAFKiller NightBuild/Repositories/RepositoryBase.cs
+++ DAFKiller NightBuild/Repositories/RepositoryBase.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MySqlConnector;
+using System.Data.Odbc;
 
 namespace DAFKiller_NightBuild.Repositories
 {
@@ -12,13 +13,19 @@ namespace DAFKiller_NightBuild.Repositories
     public abstract class RepositoryBase
     {
         private readonly string _userDBconnectionString;
+        private readonly string _davie3DBConnectionString;
         public RepositoryBase()
         {
-            _userDBconnectionString = DAFKiller.Cryptography.TripleDESEncryptDecrypt.Decrypt(ConfigurationManager.ConnectionStrings["usersDBConnectionString"].ConnectionString, "dafkiller"); ;
+            _userDBconnectionString = DAFKiller.Cryptography.TripleDESEncryptDecrypt.Decrypt(ConfigurationManager.ConnectionStrings["usersDBConnectionString"].ConnectionString, "dafkiller");
+            _davie3DBConnectionString = DAFKiller.Cryptography.TripleDESEncryptDecrypt.Decrypt(ConfigurationManager.ConnectionStrings["davie3DBConnectionString"].ConnectionString, "dafkiller");
         }
         protected MySqlConnection GetAutorizationDBConnection()
         {
             return new MySqlConnection(_userDBconnectionString);
+        }
+        protected OdbcConnection GetDavie3OdbcConnection()
+        {
+            return new OdbcConnection(_davie3DBConnectionString);
         }
     }
 }
