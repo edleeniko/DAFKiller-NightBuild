b444c8c62fecbf5c9257c1c974f3adb3c1652216
EdLee <edleeniko@gmail.com>
2023-07-28 17:46:07 +0300

    #added Certificate generator

--- DevTools/mainForm.Designer.cs
+++ DevTools/mainForm.Designer.cs
@@ -29,48 +29,62 @@
         private void InitializeComponent()
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tpCrypto = new System.Windows.Forms.TabPage();
             this.label3 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tpCertificates = new System.Windows.Forms.TabPage();
+            this.tbClientID = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbExpirationDays = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.richTbInfo = new System.Windows.Forms.RichTextBox();
+            this.btnCreateCert = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
-            this.tabPage1.SuspendLayout();
+            this.tpCrypto.SuspendLayout();
+            this.tpCertificates.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tpCrypto);
+            this.tabControl1.Controls.Add(this.tpCertificates);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(647, 384);
+            this.tabControl1.Size = new System.Drawing.Size(710, 558);
             this.tabControl1.TabIndex = 0;
             // 
-            // tabPage1
+            // tpCrypto
             // 
-            this.tabPage1.Controls.Add(this.label3);
-            this.tabPage1.Controls.Add(this.textBox3);
-            this.tabPage1.Controls.Add(this.label2);
-            this.tabPage1.Controls.Add(this.label1);
-            this.tabPage1.Controls.Add(this.textBox2);
-            this.tabPage1.Controls.Add(this.textBox1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(639, 358);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "CryptoStrings";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tpCrypto.Controls.Add(this.label3);
+            this.tpCrypto.Controls.Add(this.textBox3);
+            this.tpCrypto.Controls.Add(this.label2);
+            this.tpCrypto.Controls.Add(this.label1);
+            this.tpCrypto.Controls.Add(this.textBox2);
+            this.tpCrypto.Controls.Add(this.textBox1);
+            this.tpCrypto.Location = new System.Drawing.Point(4, 22);
+            this.tpCrypto.Name = "tpCrypto";
+            this.tpCrypto.Padding = new System.Windows.Forms.Padding(3);
+            this.tpCrypto.Size = new System.Drawing.Size(702, 532);
+            this.tpCrypto.TabIndex = 0;
+            this.tpCrypto.Text = "CryptoStrings";
+            this.tpCrypto.UseVisualStyleBackColor = true;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(21, 193);
+            this.label3.Location = new System.Drawing.Point(18, 267);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(112, 20);
             this.label3.TabIndex = 5;
@@ -79,16 +93,16 @@
             // textBox3
             // 
             this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox3.Location = new System.Drawing.Point(25, 216);
+            this.textBox3.Location = new System.Drawing.Point(22, 290);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(582, 26);
+            this.textBox3.Size = new System.Drawing.Size(655, 26);
             this.textBox3.TabIndex = 4;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(21, 104);
+            this.label2.Location = new System.Drawing.Point(18, 178);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(111, 20);
             this.label2.TabIndex = 2;
@@ -98,7 +112,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(21, 23);
+            this.label1.Location = new System.Drawing.Point(18, 97);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(73, 20);
             this.label1.TabIndex = 2;
@@ -107,32 +121,158 @@
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox2.Location = new System.Drawing.Point(25, 127);
+            this.textBox2.Location = new System.Drawing.Point(22, 201);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(582, 26);
+            this.textBox2.Size = new System.Drawing.Size(655, 26);
             this.textBox2.TabIndex = 1;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox1.Location = new System.Drawing.Point(25, 50);
+            this.textBox1.Location = new System.Drawing.Point(22, 124);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(582, 26);
+            this.textBox1.Size = new System.Drawing.Size(655, 26);
             this.textBox1.TabIndex = 0;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // tpCertificates
+            // 
+            this.tpCertificates.Controls.Add(this.richTbInfo);
+            this.tpCertificates.Controls.Add(this.btnCreateCert);
+            this.tpCertificates.Controls.Add(this.label7);
+            this.tpCertificates.Controls.Add(this.tbPassword);
+            this.tpCertificates.Controls.Add(this.label6);
+            this.tpCertificates.Controls.Add(this.tbEmail);
+            this.tpCertificates.Controls.Add(this.label5);
+            this.tpCertificates.Controls.Add(this.tbExpirationDays);
+            this.tpCertificates.Controls.Add(this.label4);
+            this.tpCertificates.Controls.Add(this.tbClientID);
+            this.tpCertificates.Location = new System.Drawing.Point(4, 29);
+            this.tpCertificates.Name = "tpCertificates";
+            this.tpCertificates.Padding = new System.Windows.Forms.Padding(3);
+            this.tpCertificates.Size = new System.Drawing.Size(702, 525);
+            this.tpCertificates.TabIndex = 1;
+            this.tpCertificates.Text = "Certificates";
+            this.tpCertificates.UseVisualStyleBackColor = true;
+            // 
+            // tbClientID
+            // 
+            this.tbClientID.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbClientID.Location = new System.Drawing.Point(39, 51);
+            this.tbClientID.Margin = new System.Windows.Forms.Padding(4);
+            this.tbClientID.Name = "tbClientID";
+            this.tbClientID.Size = new System.Drawing.Size(268, 22);
+            this.tbClientID.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(36, 31);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Client ID:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(36, 103);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Expiration (days):";
+            // 
+            // tbExpirationDays
+            // 
+            this.tbExpirationDays.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbExpirationDays.Location = new System.Drawing.Point(39, 123);
+            this.tbExpirationDays.Margin = new System.Windows.Forms.Padding(4);
+            this.tbExpirationDays.Name = "tbExpirationDays";
+            this.tbExpirationDays.Size = new System.Drawing.Size(268, 22);
+            this.tbExpirationDays.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(370, 31);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 16);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "E-mail:";
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbEmail.Location = new System.Drawing.Point(373, 51);
+            this.tbEmail.Margin = new System.Windows.Forms.Padding(4);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(268, 22);
+            this.tbEmail.TabIndex = 8;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(370, 103);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(148, 16);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Password for certificate:";
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbPassword.Location = new System.Drawing.Point(373, 123);
+            this.tbPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(268, 22);
+            this.tbPassword.TabIndex = 10;
+            // 
+            // richTbInfo
+            // 
+            this.richTbInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTbInfo.Location = new System.Drawing.Point(39, 153);
+            this.richTbInfo.Margin = new System.Windows.Forms.Padding(4);
+            this.richTbInfo.Name = "richTbInfo";
+            this.richTbInfo.Size = new System.Drawing.Size(606, 304);
+            this.richTbInfo.TabIndex = 13;
+            this.richTbInfo.Text = "";
+            // 
+            // btnCreateCert
+            // 
+            this.btnCreateCert.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCreateCert.Location = new System.Drawing.Point(377, 465);
+            this.btnCreateCert.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCreateCert.Name = "btnCreateCert";
+            this.btnCreateCert.Size = new System.Drawing.Size(269, 42);
+            this.btnCreateCert.TabIndex = 12;
+            this.btnCreateCert.Text = "Create certificates";
+            this.btnCreateCert.UseVisualStyleBackColor = true;
+            this.btnCreateCert.Click += new System.EventHandler(this.btnCreateCert_Click_1);
             // 
             // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(647, 384);
+            this.ClientSize = new System.Drawing.Size(710, 558);
             this.Controls.Add(this.tabControl1);
             this.Name = "mainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DevTools";
+            this.Load += new System.EventHandler(this.mainForm_Load);
             this.tabControl1.ResumeLayout(false);
-            this.tabPage1.ResumeLayout(false);
-            this.tabPage1.PerformLayout();
+            this.tpCrypto.ResumeLayout(false);
+            this.tpCrypto.PerformLayout();
+            this.tpCertificates.ResumeLayout(false);
+            this.tpCertificates.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -140,13 +280,24 @@
         #endregion
 
         private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tpCrypto;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TabPage tpCertificates;
+        public System.Windows.Forms.TextBox tbClientID;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbExpirationDays;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbEmail;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.RichTextBox richTbInfo;
+        private System.Windows.Forms.Button btnCreateCert;
     }
 }
 
